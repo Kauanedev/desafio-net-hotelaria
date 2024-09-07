22b357f17a7f68ce8cfd613720420a1451bd98b3
Kauane Santos <kauanesantos.coding@gmail.com>
2024-09-07 13:50:39 -0300

    fix: change class name

--- models/Guest.cs
+++ models/Guest.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace desafio_net_hotelaria.models
 {
-    public class Pessoa(string name, string lastName)
+    public class Guest(string name, string lastName)
     {
         public string Name { get; set; } = name;
         public string LastName { get; set; } = lastName;
